13570f891f336fe7de19b3dac6f920b929e498e6
CIANDT\lurian <lurian@ciandt.com>
2022-04-06 16:31:58 -0300

    merge

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,11 +3,15 @@
 build.gradle,f/0/f07866736216be0ee2aba49e392191aeae700a35
 P
  app/src/main/AndroidManifest.xml,8/c/8c55c3ccc257e5907959013f99656e4c8ec3903e
-l
-<app/src/main/java/br/com/estudo/plantmanager/SplashScreen.kt,c/f/cfe460013b655bd0e198170859dea7f9c15fb42c
 q
 Aapp/src/main/java/br/com/estudo/plantmanager/HowToNameFragment.kt,8/2/821025d4f1689be7af8421be03a28ae52752b2a3
 g
 7app/src/main/java/br/com/estudo/plantmanager/Welcome.kt,c/1/c154c89b822989a1b0f833bd4858a2d1c5ebd8cf
 m
 =app/src/main/java/br/com/estudo/plantmanager/ReadyFragment.kt,4/6/46d6b3fa46e9c8a8388013ad862355d6f9e3b976
+i
+9app/src/main/java/br/com/estudo/plantmanager/My_plants.kt,e/7/e72b17c5d07155dce40cb5d3bf79f1c927933e3c
+h
+8app/src/main/java/br/com/estudo/plantmanager/NewPlant.kt,b/b/bb93cf8374a14fd329e95935b823e90f71c664f5
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
--- app/src/main/java/br/com/estudo/plantmanager/HomePage.kt
+++ app/src/main/java/br/com/estudo/plantmanager/HomePage.kt
@@ -2,12 +2,36 @@ package br.com.estudo.plantmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import br.com.estudo.plantmanager.databinding.ActivityHomePageBinding
 
 class HomePage : AppCompatActivity() {
 
+    private lateinit var binding: ActivityHomePageBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu (menu : Menu) : Boolean {
+        val inflater : MenuInflater = menuInflater
+        inflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.my_plants -> {
+                //inflar a tela de minhas plantas
+            }
+            R.id.new_plant -> {
+                //inflar a tela de nova planta
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
